04b0d96d9c47c8431937c7a69ea36e2f8b613450
oldPumpkinlala <helenharry@yeah.net>
2020-04-23 10:59:44 +0800

    third

--- schoolidlestore/src/main/java/web/controller/OrderController.java
+++ schoolidlestore/src/main/java/web/controller/OrderController.java
@@ -1,4 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import web.service.OrderService;
+
+@Controller
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
 }
